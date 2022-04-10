1b4aa645f84b67290d0a432d87b3f9958d2278a6
Nikolay Makeev <reso1@yandex.ru>
2022-04-10 23:01:33 +0300

    Сокеты первая версия

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int port = 8089;
+
+        while (true) {
+            ServerSocket serverSocket = new ServerSocket(port);
+            // ждем подключения
+            Socket connection = serverSocket.accept();
+            // отправляем сообщения
+            PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
+            // получаем сообщения
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            System.out.printf("Принято новое соединение на порту %d%n", connection.getPort());
+            // ждем строку от клиента
+            String name = in.readLine();
+            // отвечаем клиенту
+            out.println(String.format(" Привет, %s, твой порт %d", name, connection.getPort()));
+            // закрываем соединение
+            serverSocket.close();
+        }
+
+
+    }
+
+
+}
